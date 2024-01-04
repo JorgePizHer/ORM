fea70b9c64df2e88cea5ef2c465a8cd4a454d7bc
JorgePizHer <jpizarroh166@alumnos.imf.com>
2024-01-04 15:40:02 +0100

    4

--- orm.py
+++ orm.py
@@ -1,22 +1,45 @@
 import tkinter as tk
 import random
 
+personas = []
+numeropersonas = 20
+
+
 class Persona:
     def __init__(self):
-        self.posx = random.randint(0,512)
-        self.posy = random.randint(0,512)
+        self.posx = random.randint(0,1024)
+        self.posy = random.randint(0,1024)
         self.radio = 30
+        self.direccion = 0
+        self.color = "blue"
+        self.entidad = "" #identificador
 
     def dibuja(self):
-        lienzo.create_oval(self.posx-self.radio/2,
+        self.entidad = lienzo.create_oval(
+                           self.posx-self.radio/2,
                            self.posy-self.radio/2,
                            self.posx+self.radio/2,
-                           self.posy+self.radio/2,fill="red")
+                           self.posy+self.radio/2,
+                           fill=self.color)
+    def mueve(self):
+        lienzo.move(self.entidad,500,0)
+        
 
 raiz = tk.Tk()
 
 lienzo = tk.Canvas (width=1024,height=1024)
 lienzo.pack()
+
+for i in range(0,numeropersonas):
+    personas.append(Persona())
+
+for persona in personas:
+    persona.dibuja()
+    
+for persona in personas:
+    persona.mueve()
+
+print (personas)
 
 persona = Persona()
 persona.dibuja()
